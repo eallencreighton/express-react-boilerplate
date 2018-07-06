22b5a72f099660e60f7060b865afb8d6c9c821ce
Esme Allen-Creighton <eallencreighton@gmail.com>
2018-07-06 16:05:42 -0400

    adding LETS materials

--- src/App.js
+++ src/App.js
@@ -15,19 +15,27 @@ class App extends Component {
   // THIS IS CALLED A TRY CHECK WITH ASYNC AWAIT< REPLACES PROMISE
   async componentDidMount () {
     try {
-      const response = await axios.get('/healthcheck')
-      console.log('hello')
+      const response = await axios.post('/login')
+      //console.log('hello')
       //  won't run until promise is complete!
       console.log(response.data)
     } catch (error) {
       console.log(error)
     }
+    // try {
+    //   const response = await axios.post('/login')
+    //   //console.log('hello')
+    //   //  won't run until promise is complete!
+    //   console.log(response.data)
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
   render () {
     // if (this.state.error) return <div>Somtheing went wrong!</div>
     // This is what you would have to do.... if no try catch
     return (
-      <div>Hello there,</div>
+      <div>Hello there</div>
     )
   }
 }
